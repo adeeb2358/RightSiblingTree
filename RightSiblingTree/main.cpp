3ed375dca65387ccc22c3b63e7c590ead509f2fd
Adeeb Mohammed <adeeb2358@gmail.com>
2020-04-30 15:20:43 +0900

    right sibling tree o(n) time complexity and o(d) space

--- RightSiblingTree/main.cpp
+++ RightSiblingTree/main.cpp
@@ -4,12 +4,51 @@
 //
 //  Created by adeeb mohammed on 27/04/20.
 //  Copyright © 2020 adeeb mohammed. All rights reserved.
-//
+// time complexity is o(n) space complexity is  o(d) o(log(n)) for perfectly balanced tree
 
 #include <iostream>
 
+// This is the class of the input root. Do not edit it.
+class BinaryTree {
+public:
+  int value;
+  BinaryTree *left = NULL;
+  BinaryTree *right = NULL;
+
+BinaryTree(int value);
+};
+
+void mutateTree(BinaryTree *tree,
+                BinaryTree *parent,
+                bool isLeftChild){
+    if(tree == nullptr){
+        return;
+    }
+    
+    auto left  = tree->left;
+    auto right = tree->right;
+    
+    mutateTree(left, tree, true);
+    if (parent == nullptr) {
+        tree->right = nullptr;
+    }else if(isLeftChild){
+        tree->right = parent->right;
+    }else {
+        if (parent->right  == nullptr) {
+            tree->right = nullptr;
+        }else{
+            tree->right = parent->right->left;
+        }
+    }
+    mutateTree(right, tree, false);
+}
+
+BinaryTree *rightSiblingTree(BinaryTree *root) {
+    mutateTree(root, nullptr, false);
+    return root;
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Making the Binary Tree a Right Sibling Tree" << std::endl;
     return 0;
 }
